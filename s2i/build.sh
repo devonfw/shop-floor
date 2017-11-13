f68501364e6917da1b78466f31ac00c8605fb6da
Dacal Cantos <jorge.dacal-cantos@capgemini.com>
2017-11-13 10:22:02 +0100

    add examples to build with params in file

--- s2i/build.sh
+++ s2i/build.sh
@@ -5,8 +5,8 @@ oc new-project devonfw --display-name='DevonFW' --description='DevonFW'
 
 # Create base-images and add them to DevonFW project
 # this files are private, to share it, you must enter in Git with a valid user, open the file and press RAW Button to generate a valid token
-oc create -f https://raw.githubusercontent.com/devonfw/devonfw-shop-floor/master/s2i/java/s2i-devonfw-java-imagestream.json?token=AfL84JdanagHUMCYJ3NFr7UxDf4z_xAQks5aCYOBwA%3D%3D --namespace=devonfw
-oc create -f https://raw.githubusercontent.com/devonfw/devonfw-shop-floor/master/s2i/angular/s2i-devonfw-angular-imagestream.json?token=AfL84CSy0zE01pC9qdcCkOTO1j4aRkPyks5aCYNUwA%3D%3D --namespace=devonfw
+oc create -f https://raw.githubusercontent.com/devonfw/devonfw-shop-floor/master/s2i/java/s2i-devonfw-java-imagestream.json?token= --namespace=devonfw
+oc create -f https://raw.githubusercontent.com/devonfw/devonfw-shop-floor/master/s2i/angular/s2i-devonfw-angular-imagestream.json?token= --namespace=devonfw
 
 # Build base-images in DevonFW project
 oc start-build s2i-devonfw-java --namespace=devonfw
