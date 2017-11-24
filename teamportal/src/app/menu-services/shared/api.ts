ddc402c734d267c1c4f5dddf91d47da7f7307540
Dacal Cantos <jorge.dacal-cantos@capgemini.com>
2017-11-24 12:25:46 +0100

    API Operations

--- teamportal/src/app/menu-services/shared/api.ts
+++ teamportal/src/app/menu-services/shared/api.ts
@@ -2,24 +2,29 @@ import { environment } from '../../../environments/environment';
 
 export class API {
     private AUTH = 'oauth/';
-    private OPENSHIFT = 'oapi/';
-    private KUBERNETES = 'apis/';
+    private OPENSHIFT = 'oapi/v1/';
+    private KUBERNETES = 'apis/v1/';
 
     // GET
     public REQUEST_TOKEN = environment.clusterURL + this.AUTH + 'token/request';
-    public REQUEST_PROJECTS = environment.clusterURL + this.OPENSHIFT + 'v1/projects';
-    public REQUEST_SERVICE_ROUTES = 'route.openshift.io/v1/namespaces/$NAMESPACE/routes';
-    public REQUEST_SECRET = '';
+    public REQUEST_PROJECTS = environment.clusterURL + this.OPENSHIFT + 'projects';
+    public REQUEST_TEMPLATE = environment.clusterURL + this.OPENSHIFT + 'namespace/$NAMESPACE/templates/$NAME';
+    public REQUEST_ROUTES = environment.clusterURL + this.OPENSHIFT + 'namespace/$NAMESPACE/routes/$NAME';
 
     // POST
-    public CREATE_PROJECT = '';
-    public CREATE_APP = '';
-    public BUILD_APP = '';
-    public CREATE_SECRET = '';
-    public MAKE_SECRET_BUILD_SECRET = '';
+    public CREATE_PROJECT = environment.clusterURL + this.OPENSHIFT + 'projectrequests';
+    public PROCESS_TEMPLATE = environment.clusterURL + this.OPENSHIFT + 'namespace/$NAMESPACE/processedtemplates';
+    public CREATE_BUILDCONFIG = environment.clusterURL + this.OPENSHIFT + 'namespace/$NAMESPACE/buildconfigs';
+    public CREATE_IMAGESTREAM = environment.clusterURL + this.OPENSHIFT + 'namespace/$NAMESPACE/imagestreams';
+    public CREATE_DEPLOYMENTCONFIG = environment.clusterURL + this.OPENSHIFT + 'namespace/$NAMESPACE/deploymentconfigs';
+    public CREATE_ROUTE = environment.clusterURL + this.OPENSHIFT + 'namespace/$NAMESPACE/routes';
+    public CREATE_SERVICE = environment.clusterURL + this.OPENSHIFT + 'namespace/$NAMESPACE/services';
+    public CREATE_SECRET = environment.clusterURL + this.KUBERNETES + 'namespace/$NAMESPACE/secrets';
+    public BUILD_APP = environment.clusterURL + this.OPENSHIFT + 'namespace/$NAMESPACE/buildconfigs/$NAME/instantiate';
 
     // PATCH
-    public ADD_SECRET_TO_BUILDER_SA = this.KUBERNETES + '';
+    public PATCH_SERVICEACCOUNT = environment.clusterURL + this.KUBERNETES + 'namespace/$NAMESPACE/serviceaccounts/$NAME';
+    public PATCH_BUILDCONFIG = environment.clusterURL + this.KUBERNETES + 'namespace/$NAMESPACE/buildconfigs/$NAME';
 
     constructor() {}
 
--- teamportal/src/app/menu-services/shared/openshift.service.ts
+++ teamportal/src/app/menu-services/shared/openshift.service.ts
@@ -7,6 +7,7 @@ import { Service } from './service';
 import { SERVICESLIST, MYSERVICES } from './mock-services';
 import { Response } from '@angular/http/src/static_response';
 import { catchError, map, tap } from 'rxjs/operators';
+import * as INTERFACES from './models';
 
 // Service to call Cluster's REST API operations
 // based on https://docs.google.com/spreadsheets/d/1Cv6nfMDH3TqGRUjyCdZZ6k4W4trFAD8tk_5HXskC6BQ/edit?usp=sharing
@@ -32,70 +33,178 @@ export class OpenShiftService {
     }
 
     // GET
-
-    requestToken(username: string, password: string): Observable<any> {
-        const bearerparam = 'Basic ' + btoa(username + ':' + password);
-        return this.http.get(this.API.REQUEST_TOKEN, {
-            headers: new HttpHeaders({
-                'Authorization': bearerparam,
-            }),
-            responseType: 'text'
-        });
-    }
-
-    // getProjects
-    requestProjects(): Observable<any> {
-        return this.http.get(this.API.REQUEST_PROJECTS, {
+    private get(route): Observable<any> {
+        return this.http.get(route, {
             headers: new HttpHeaders({
                 'Authorization': 'Bearer ' + localStorage.getItem('token'),
             })
         });
     }
 
-    requestServiceRoutes(): Observable<any> {
-        return this.http.get(this.API.REQUEST_SERVICE_ROUTES);
+    requestToken(params: INTERFACES.BasicAuth){
+        const basicAuth = 'Basic ' + btoa(params.username + ':' + params.password);
+        return this.http.get(this.API.REQUEST_TOKEN, {
+            headers: new HttpHeaders({
+                'Authorization': basicAuth,
+            }),
+            responseType: 'text'
+        });
+    }
+    
+    // requestToken(username: string, password: string): Observable<any> {
+    //     const bearerparam = 'Basic ' + btoa(username + ':' + password);
+    //     return this.http.get(this.API.REQUEST_TOKEN, {
+    //         headers: new HttpHeaders({
+    //             'Authorization': bearerparam,
+    //         }),
+    //         responseType: 'text'
+    //     });
+    // }
+    
+
+    requestProjects(): Observable<any> {
+        return this.get(this.API.REQUEST_PROJECTS);
     }
 
-    requestSecret(): Observable<any> {
-        return this.http.get(this.API.REQUEST_SECRET);
+    requestTemplate(params: INTERFACES.RouteNameAndNamespace): Observable<any> {
+        const route = this.API.REQUEST_TEMPLATE;
+        route.replace("$NAME", params.name);
+        route.replace("$NAMESPACE", params.namespace);
+        return this.get(route);
+    }
+
+    requestAllRoutes(params: INTERFACES.RouteNamespace): Observable<any> {
+        const route = this.API.REQUEST_ROUTES;
+        route.replace("$NAMESPACE", params.namespace);
+        route.replace("/$NAME", "");
+        return this.get(route);
+    }
+
+    requestRoutes(params: INTERFACES.RouteNameAndNamespace): Observable<any> {
+        const route = this.API.REQUEST_ROUTES;
+        route.replace("$NAME", params.name);
+        route.replace("$NAMESPACE", params.namespace);
+        return this.get(route);
     }
 
     // POST
-
-    createProject(name: string, namespace: string, description: string, displayname: string): Observable<any> {
-        return this.http.post(this.API.CREATE_PROJECT, {
-            'name': name,
-            'namespace': namespace,
-            'description': description,
-            'displayname': displayname
+    private post(route, body): Observable<any> {
+        return this.http.POST(route, body, {
+            headers: new HttpHeaders({
+                'Authorization': 'Bearer ' + localStorage.getItem('token'),
+                'Content-Type': 'application/json'
+            })
         });
     }
 
-    createApp(data: any): Observable<any> {
-        return this.http.post(this.API.CREATE_APP, {'data': data});
+    createProject(params: INTERFACES.CreateProject): Observable<any> {
+        const body = {
+            'displayName': params.displayName,
+            'description': params.displayName,
+            'kind': "ProjectRequest",
+            'metadata': {
+              'name': params.name,
+              'namespace': params.name
+            }
+        }
+        return this.post(this.API.CREATE_PROJECT, body);
     }
 
-    buildApp(data: any): Observable<any> {
-        return this.http.post(this.API.BUILD_APP, {'data': data});
+    processedTemplate(params: INTERFACES.RouteNamespaceAndBodyJSON): Observable<any> {
+        const route = this.API.PROCESS_TEMPLATE;
+        route.replace("$NAMESPACE", params.namespaceRoute);
+        return this.post(route, params.bodyJSON);
     }
 
-    createSecret(data: any): Observable<any> {
-        return this.http.post(this.API.CREATE_SECRET, {'data': data});
+    createBuildConfig(params: INTERFACES.RouteNamespaceAndBodyJSON): Observable<any> {
+        const route = this.API.CREATE_BUILDCONFIG;
+        route.replace("$NAMESPACE", params.namespaceRoute);
+        return this.post(route, params.bodyJSON);
     }
 
-    makeSecretBuildSecret(data: any): Observable<any> {
-        return this.http.post(this.API.MAKE_SECRET_BUILD_SECRET, {'data': data});
+    createImageStream(params: INTERFACES.RouteNamespaceAndBodyJSON): Observable<any> {
+        const route = this.API.CREATE_IMAGESTREAM;
+        route.replace("$NAMESPACE", params.namespaceRoute);
+        return this.post(route, params.bodyJSON);
+    }
+
+    createDeploymentConfig(params: INTERFACES.RouteNamespaceAndBodyJSON): Observable<any> {
+        const route = this.API.CREATE_DEPLOYMENTCONFIG;
+        route.replace("$NAMESPACE", params.namespaceRoute);
+        return this.post(route, params.bodyJSON);
+    }
+
+    createRoutes(params: INTERFACES.RouteNamespaceAndBodyJSON): Observable<any> {
+        const route = this.API.CREATE_ROUTE;
+        route.replace("$NAMESPACE", params.namespaceRoute);
+        return this.post(route, params.bodyJSON);
+    }
+
+    createServices(params: INTERFACES.RouteNamespaceAndBodyJSON): Observable<any> {
+        const route = this.API.CREATE_SERVICE;
+        route.replace("$NAMESPACE", params.namespaceRoute);
+        return this.post(route, params.bodyJSON);
+    }
+    
+    createSecret(params: INTERFACES.CreateSecret): Observable<any> {
+        const route = this.API.CREATE_SECRET;
+        route.replace("$NAMESPACE", params.namespace);
+        const body = {
+            'kind': "Secret",
+            'metadata': {
+                'name': params.name,
+            },
+            'type': "kubernetes.io/ssh-auth",
+            'ssh-privatekey': params.sshprivatekey
+        }
+        return this.post(route, body);
+    }
+
+    buildApp(params: INTERFACES.RouteNameAndNamespace): Observable<any> {
+        const route = this.API.CREATE_SECRET;
+        route.replace("$NAMESPACE", params.namespace);
+        route.replace("$NAME", params.name);
+        const body = {
+            'kind': "BuildRequest",
+            'metadata': {
+                'name': params.name,
+            }
+        }
+        return this.post(route, body);
     }
 
     // PATCH
-    addSecretToBuilderSA(secretname: string): Observable<any> {
-        return this.http.patch(this.API.ADD_SECRET_TO_BUILDER_SA, {
-            'op': 'add',
-            'path': '/secrets/-',
-            'value': {
-                'name': secretname
-            }
+    private patch(route,body): Observable<any> {
+        return this.http.PATCH(route, body, {
+            headers: new HttpHeaders({
+                'Authorization': 'Bearer ' + localStorage.getItem('token'),
+                'Content-Type': 'application/json-patch+json'
+            }),
         });
+    }
+
+    addSecretToBuilderSA(params: INTERFACES.Secret): Observable<any> {
+        const route = this.API.PATCH_SERVICEACCOUNT;
+        route.replace("$NAME", params.name);
+        route.replace("$NAMESPACE", params.namespace);
+        const body = [{
+            "op": "add",
+            "path": "/secrets/-",
+            "value": { "name": params.nameSecret }
+        }];
+        return this.patch(route, body);
+    }
+
+    addSecretToBuildconfig(params: INTERFACES.Secret): Observable<any> {
+        const route = this.API.PATCH_BUILDCONFIG;
+        route.replace("$NAME", params.name);
+        route.replace("$NAMESPACE", params.namespace);
+        const body = [{
+            "op": "add",
+            "path": "/spec/source/sourceSecret",
+            "value": { "name": params.nameSecret }
+        }];
+        return this.patch(route, body);
     }
 
 }
