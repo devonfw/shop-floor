27d5eef2ab262af2e26470f0511e556890e4f110
Jorge Dacal Cantos <32701664+Jorge-Dacal@users.noreply.github.com>
2018-01-09 13:52:29 +0100

    Update environment.prod.ts
    change route to remote machine

--- dsf4openshift/teamportal/src/environments/environment.prod.ts
+++ dsf4openshift/teamportal/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  clusterURL: 'https://10.0.75.2:8443/'
+  // clusterURL: 'https://10.0.75.2:8443/'
+  clusterURL: 'https://10.68.26.163:8443/'
 };
